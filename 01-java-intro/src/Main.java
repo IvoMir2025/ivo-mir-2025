ca7a775da366561f265cbd1f248d78c03d590fad
IvoMir2025 <ivo.mirtchev37@gmail.com>
2025-06-17 10:48:55 +0300

    01-java-intro initial commit

--- 01-java-intro/src/Main.java
+++ 01-java-intro/src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import course.spring.model.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello from Java!\n");
+        List<User> users = new ArrayList<>();
+        users.add(new User("John", "Smith", "r","g"));
+        users.add(new User("Ivan", "Petrov", "g","d"));
+        users.add(new User("Maria", "Hristova", "g","g"));
+  //      users.forEach(System.out::println);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for(User p : users){
+            System.out.println(p);
+        }
+        System.out.println("=================================");
+        for(int i = 0; i < users.size(); i++) {
+            System.out.println(users.get(i));
         }
     }
 }
