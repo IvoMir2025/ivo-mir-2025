d51ed6012ca98d07da49726ddd50a3b96c1e881e
IvoMir2025 <ivo.mirtchev37@gmail.com>
2025-06-24 11:57:59 +0300

    Spring Bean configurations

--- 03-spring-gradle/src/main/java/course/spring/domain/impl/UserServiceImpl.java
+++ 03-spring-gradle/src/main/java/course/spring/domain/impl/UserServiceImpl.java
@@ -4,18 +4,50 @@ import course.spring.dao.UserRepository;
 import course.spring.domain.UserService;
 import course.spring.exception.NonexistingEntityException;
 import course.spring.model.User;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.java.Log;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-public class UserServiceImpl implements UserService {
+@Log
+public class UserServiceImpl implements UserService, BeanNameAware, ApplicationContextAware {
     private UserRepository userRepo;
+    private String beanName;
+    private ApplicationContext ctx;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepo) {
         this.userRepo = userRepo;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        beanName = name;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ctx = applicationContext;
+    }
+
+
+    @PostConstruct
+    public void afterInit() throws Exception {
+        log.info(String.format("!!!!! Bean '%s' created.", beanName));
+        var repo = ctx.getBean(UserRepository.class);
+        log.info("!!!!!!!!!!!!! User repository: " + repo.toString());
+        var userNames = repo.findAll().stream()
+                .map(User::getName).collect(Collectors.joining(", "));
+        log.info(String.format("!!!!! Users:", userNames));
     }
 
     @Override
